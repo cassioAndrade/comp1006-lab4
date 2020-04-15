d90934fab326740edae6bcdf6f942397beb0724f
cassioAndrade <58600993+cassioAndrade@users.noreply.github.com>
2020-04-14 23:24:43 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+//connect to db
+$conn = new PDO("mysql:host=172.31.22.43;dbname=Cassio200439928", "Cassio200439928", "U4xAywjW1u");
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
---
+++ search.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+
+require_once ('header.php');
+
+?>
+<form method="POST" action="search-results.php">
+<div>
+	<label>Keywords</label>
+	<input name="keywords" />
+</div>
+<button>Search</button>
+</form>
+</body>
+</html>
